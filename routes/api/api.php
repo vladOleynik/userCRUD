42fdfed6dae21e4018768d5e45a19ea5f9d9ee32
fredis <oleynikprog@gmail.com>
2020-06-17 14:46:11 +0300

    ADD: base structure of routers and add api user controller

--- routes/api/api.php
+++ routes/api/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>config('api.version'), 'namespace'=>'API'], function (){
+    Route::namespace('User')
+        ->group(base_path('routes/api/user.php'));
+});
